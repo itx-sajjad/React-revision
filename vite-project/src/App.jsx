bbbcd0eaf648d0827de6a8a19506bb4264828cc7
itx-sajjad <155973920+itx-sajjad@users.noreply.github.com>
2026-09-06 04:45:17 +0500

    .

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/navbar/Navbar';
+import Button from './components/button/Button';
+function App(){
+  return(
+    <>
+    <Navbar/>
+    <div>Hello</div>
+    <Button/>
+    <Button/>
+    <Button/>
+    </>
+  )
+}
+export default App;
---
+++ vite-project/src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css'
+export default function Navbar (){
+    return(
+        <>
+        <div className='nav'>
+            <ul>
+        <li>about</li>
+        <li>contact</li>
+        <li>login</li>
+        </ul>
+        </div>
+        </>
+    )
+}
